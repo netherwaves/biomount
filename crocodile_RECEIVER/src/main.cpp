f5b3688eed9c04757418891293f0a9fa34d2c418
netherwaves <avery@netherwaves.com>
2022-06-16 17:03:24 -0400

    add teensy 4 i2c wire lib; make basic i2c sketch w/ ESP32

--- crocodile_RECEIVER/src/main.cpp
+++ crocodile_RECEIVER/src/main.cpp
@@ -1,9 +1,16 @@
 #include <Arduino.h>
+#include <Wire.h>
 
 void setup() {
   // put your setup code here, to run once:
+  Wire.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Wire.beginTransmission(0x08);
+  Wire.write(8);
+  Wire.endTransmission();
+
+  delay(1000);
 }
--- crocodile_SOUND/src/main.cpp
+++ crocodile_SOUND/src/main.cpp
@@ -1,9 +1,22 @@
-#include <Arduino.h>
+#include <i2c_driver_wire.h>
+
+void receiveEvt(int howMany);
 
 void setup() {
+  Serial.begin(9600);
+
   // put your setup code here, to run once:
+  Wire1.begin(0x08);
+  Wire1.onReceive(receiveEvt);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+}
+
+void receiveEvt(int howMany) {
+  while (Wire1.available() > 0) {
+    int i = Wire1.read();
+    Serial.println(i);
+  }
 }
